bf15f3172b5bdb4aeef55ce9289f7d695bd5b974
Jakub Rohleder <jakubrohleder@gmail.com>
2015-10-29 00:48:55 +0100

    Mario icon for demo

--- demo/app/index.constants.js
+++ demo/app/index.constants.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular.module('angularJsonapiExample')
-  /* global _: false */
+  /* global _: false, Favico: false */
     .constant('_', _)
     .constant('apiURL', 'http://localhost:3000')
-    ;
+    .constant('Favico', Favico);
 })();
--- demo/app/index.run.js
+++ demo/app/index.run.js
@@ -2,7 +2,8 @@
   'use strict';
 
   angular.module('angularJsonapiExample')
-    .run(logEvents);
+    .run(logEvents)
+    .run(marioFavicon);
 
   function logEvents(
     $rootScope,
@@ -48,6 +49,31 @@
       angular.forEach(watchers, function(watcher) {
         watcher();
       });
+    }
+  }
+
+  function marioFavicon(Favico, $interval) {
+    var favicon = new Favico({
+      animation:'slide',
+      position: 'up'
+    });
+    var up = new Image();
+    up.src = 'assets/images/mario-up.gif';
+
+    var down = new Image();
+    down.src = 'assets/images/mario-down.gif';
+
+    var isDown = true;
+
+    $interval(changeIcon, 10);
+
+    function changeIcon() {
+      isDown = !isDown;
+      if (isDown) {
+        favicon.image(down);
+      } else {
+        favicon.image(up);
+      }
     }
   }
 })();
